4d66b86db98bd4daf32be7a22ea5c9a2351c10f6
ADM <aaacoadm@gmail.com>
2026-03-25 16:59:39 +0100

    Add files via upload

---
+++ lez_1.jl
@@ -0,0 +1,16 @@
+# questo è il primo esercizio in julia lang 
+
+#esercizio scrittura script
+
+println("ciao a tutti dal primo vero programma in julia")
+println("sto imparando julia lang")
+println("mi piace molto")
+
+#esercizio variabili
+
+nome = "andrea"
+eta = 30
+altezza = 1.75
+
+println("mi chiamo $nome, ed ho $eta anni e sono alto $altezza metri")
+

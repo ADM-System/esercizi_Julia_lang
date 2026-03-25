b2bdf279f2f6bbb39a6148546fd8c4ae651ff8f7
ADM <aaacoadm@gmail.com>
2026-03-25 17:37:15 +0100

    file su esercizio con operatori aritmetici e flusso if

---
+++ lez_2.jl
@@ -0,0 +1,33 @@
+# Questa lezione è la seconda
+# e tratta gli operatori matematici
+# e il controllo del flusso
+
+# Variabili
+a = 10
+b = 20
+
+# Operatori matematici + - / * ^
+somma = a + b
+sottrazione = b - a
+
+prodotto = a * b
+divisione = b / a
+
+potenza = a ^ 2
+
+println("La somma di $a e $b è $somma")
+println("La sottrazione è $sottrazione")
+println("Il prodotto è $prodotto")
+println("La divisione è $divisione")
+println("L'elevamenti a potenza è : $potenza")
+
+
+# Controllo del flusso
+
+voto = 18
+
+if voto > 17
+    println("Complimenti, l'esame è superato")
+else
+    println("Mi dispiace, l'esame non è superato")
+end

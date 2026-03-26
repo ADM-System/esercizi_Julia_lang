d533447805b805cdae0f2296619aaeb684821bdf
ADM <aaacoadm@gmail.com>
2026-03-26 12:03:26 +0100

    lezione sui dizionari

---
+++ lez_5.jl
@@ -0,0 +1,8 @@
+# Lezione n°5 i dizionari
+
+# creiamo un dizionario di pezzi
+pezzi = Dict("mela" => 30 , "pera" => 22, "banana"=> 100)
+
+# come accedere ai dati del dizionario
+println("Questo è il primo quantitativo dell'elemento : $(pezzi["mela"])  ")
+
